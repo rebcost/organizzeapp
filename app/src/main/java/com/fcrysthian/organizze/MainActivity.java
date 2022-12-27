7d336528a5585556cad311187a0bf88e1c04b0f5
Felýpë Costa <rebelo.filipe020@gmail.com>
2022-12-26 22:59:15 -0400

    implementação firebase e Slide intro

--- app/src/main/java/com/fcrysthian/organizze/MainActivity.java
+++ app/src/main/java/com/fcrysthian/organizze/MainActivity.java
@@ -1,14 +1,45 @@
 package com.fcrysthian.organizze;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        carregarSlide();
+
+    }
+
+    private void carregarSlide() {
+
+        // Configura a visibilidade dos botões back e next
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build());
+
     }
 }
