89ff4d60aedc159beab55f2cfbf6915537009c3d
Felýpë Costa <rebelo.filipe020@gmail.com>
2022-12-28 20:24:23 -0400

    Desenvolvimento da tela de cadastro e login

---
+++ app/src/main/java/com/fcrysthian/organizze/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.fcrysthian.organizze.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fcrysthian.organizze.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/fcrysthian/organizze/activity/MainActivity.java
+++ app/src/main/java/com/fcrysthian/organizze/activity/MainActivity.java
@@ -1,7 +1,12 @@
-package com.fcrysthian.organizze;
+package com.fcrysthian.organizze.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.fcrysthian.organizze.R;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -40,6 +45,21 @@ public class MainActivity extends IntroActivity {
                 .background(android.R.color.white)
                 .fragment(R.layout.intro_4)
                 .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
+                .build());
+
+    }
+
+    public void btnEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btnCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
 
     }
 }
